064ee163d1a6a6ef6f05436aaa70c468418e2479
LuanS <luansellera98@gmail.com>
2024-09-24 23:05:28 -0300

    new: adicionando tsconfig.json no server

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const routes = express.Router();
 
-routes.post("/posts", (req, res) => {
-  return response.json({ message: "Hello World" });
+routes.post("/users", (req, res) => {
+  return res.json({ message: "Hello World" });
 });
 
 export default routes;
