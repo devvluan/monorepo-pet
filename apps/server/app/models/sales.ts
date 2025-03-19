b118e42c8ae221255f964a7ea91298f5e178b350
LuanS <luansellera98@gmail.com>
2025-03-18 22:01:28 -0300

    feat(models): Criado models | migrations | packages

---
+++ apps/server/app/models/sales.ts
@@ -0,0 +1,36 @@
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { DateTime } from 'luxon'
+
+export class Sales extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column({ columnName: 'profileId', serializeAs: 'profileId' })
+  declare profileId: number
+
+  @column()
+  declare client: string
+
+  @column()
+  declare product: string
+
+  @column()
+  declare quantity: number
+
+  @column()
+  declare price: number
+
+  @column()
+  declare formsPayment: string
+
+  @column.dateTime({ autoCreate: true, columnName: 'created_at', serializeAs: 'created_at' })
+  declare created_at: DateTime
+
+  @column.dateTime({
+    autoCreate: true,
+    autoUpdate: true,
+    columnName: 'updated_at',
+    serializeAs: 'updated_at',
+  })
+  declare updated_at: DateTime
+}
